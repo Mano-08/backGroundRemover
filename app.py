b435b4b4696f3b2bf55a192061af044d2107714a
Mano <mano.26q@gmail.com>
2023-02-23 17:32:20 +0530

    initial commit

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ from cvzone.SelfiSegmentationModule import SelfiSegmentation
 cap = cv2.VideoCapture(0)
 
 # extracting the camera capture size
-width, height = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH )), int( cap.get(cv2.CAP_PROP_FRAME_HEIGHT ))
+width, height = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(
+    cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 # loading and resizing the background image
-background_image = cv2.resize(cv2.imread("bg_image.jpeg"), (width, height)) 
+background_image = cv2.resize(cv2.imread("backGround.jpg"), (width, height))
 
 # creating segmentation instance for taking the foreground (the person).
 segmentor = SelfiSegmentation()
@@ -19,18 +20,18 @@ while True:
     ret, frame = cap.read()
 
     # segmenting the image
-    segmentated_img = segmentor.removeBG(frame, background_image, threshold=0.9)
+    segmentated_img = segmentor.removeBG(
+        frame, background_image, threshold=0.9)
 
     # concatenating the images horizontally
     concatenated_img = cv2.hconcat([frame, segmentated_img])
 
-    #cv2.imshow("Camera Capture", concatenated_img)
+    # cv2.imshow("Camera Capture", concatenated_img)
     cv2.imshow("Camera Live", concatenated_img)
 
     # ending condition
     if cv2.waitKey(1) == ord('q'):
         break
 
-# relasing the sources
 cap.release()
 cv2.destroyAllWindows()
